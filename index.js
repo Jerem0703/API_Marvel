ea9ad3cf6aaa957c3371c53f91ff49b071e36f10
Ervin <ervin@github.com>
2018-11-15 17:01:46 +0100

    First commit

--- index.js
+++ index.js
@@ -2,22 +2,23 @@
 
 const program = require('commander')
 const inquirer = require('inquirer')
-const progress = require('progress');
+const progress = require('progress')
+const urljoin = require('url-join');
 const axios = require('axios')
 
 // Attribution des commandes à "Commander"
 
 program
     .version('1.0.0')
-    .option('-a, --allcharacters', 'Show all characters and associated ID ')
-    .option('-p, --characters', 'Search characters by ID or name')
+    .option('-p, --characters', 'Search characters by name and show ID and Description')
     .option('-c, --comics', 'Show comics')
     .option('-s, --series', 'Show series')
 
 program.parse(process.argv)
 
 // Requêtes API Marvel, pour récuperer selon la commande : les personnages, les comics, les séries.
-const questions = [
+
+const question_characters_name = [
     {
       type : 'input',
       name : 'personnages',
@@ -25,125 +26,93 @@ const questions = [
     }
   ];
 
+const question_comics_name = [
+    {
+      type : 'input',
+      name : 'comics',
+      message : "Le comic commence par :"
+    }
+  ];
+
 if (program.characters) {
+    
     inquirer
-    .prompt(questions).then(answers =>
+    .prompt(question_characters_name).then(answers => {
         
-        axios.get('https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=01907a0e43ea09e2dd60256479542cd3&hash=594983bea9a4bb32904aead39c6b11cf')
-            .then((responseCharacters) => {
-                const characters = responseCharacters.data.data.results
-                        
-                characters.forEach(element => {
+        url = 'characters?name=' + Object.values(answers)
+        let fullUrl = urljoin('https://gateway.marvel.com:443/v1/public', url, '&ts=1&apikey=01907a0e43ea09e2dd60256479542cd3&hash=594983bea9a4bb32904aead39c6b11cf')
+
+        axios.get(fullUrl)
+        .then((responseCharacters) => {
+            
+            const characters = responseCharacters.data.data.results
                     
-                    if (Object.values(answers) == element.id) {
-                        
-                        let bar = new progress('Téléchargement \: :percent', { total: 100 });
-                        let timer = setInterval(function () {
-                        bar.tick();
-                            if (bar.complete) {
-                                console.log("Personnage : " + element.name)
-                                console.log("Description : " + element.description)
-                                console.log("Comics disponible : " + element.comics.available)
-                                clearInterval(timer);
-                            }
-                        }, 100);
+            characters.forEach(element => {
 
-                    }
-
-                    else if (Object.values(answers) == element.name) {
-                        
-                        let bar = new progress('Téléchargement \: :percent', { total: 100 });
-                        let timer = setInterval(function () {
-                        bar.tick();
-                            if (bar.complete) {
-                                console.log("ID : " + element.id + "| Personnage : " + element.name)
-                                console.log("Description : " + element.description)
-                                console.log("Comics disponible : " + element.comics.available)
-                                clearInterval(timer);
-                            }
-                        }, 100);
-                    }
-
-                });
-                
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-)
-
-} else if (program.allcharacters) {
-        
-        axios.get('https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=01907a0e43ea09e2dd60256479542cd3&hash=594983bea9a4bb32904aead39c6b11cf')
-            .then((responseCharacters) => {
-                
-                const allcharacters = responseCharacters.data.data.results
-                
-                let bar = new progress('Téléchargement \: :percent', { total: 100 });  
+                if (Object.values(answers) == element.name) {
+                    
+                    let bar = new progress('Téléchargement \: :percent', { total: 100 });
                     let timer = setInterval(function () {
                     bar.tick();
                         if (bar.complete) {
-                            console.log(characters_array)
+                            console.log("\nID : " + element.id + " | Personnage : " + element.name)
+                            console.log("\nDescription : " + element.description)
+                            console.log("\nComics disponible : " + element.comics.available)
                             clearInterval(timer);
                         }
                     }, 100);
-                
-                characters_array = []
+                }
 
-                allcharacters.forEach(element => {
-                    
-                    characters_array.push("ID : " + element.id + " | Personnage : "+ element.name)
-
-                });
-                
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
+            });
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+})
 
 } else if (program.comics) {
+    
+    inquirer
+    .prompt(question_comics_name).then(answers => {
+        
+        url = 'comics?titleStartsWith=' + Object.values(answers)
+        let fullUrl = urljoin('https://gateway.marvel.com:443/v1/public', url, '&ts=1&apikey=01907a0e43ea09e2dd60256479542cd3&hash=594983bea9a4bb32904aead39c6b11cf')
+        console.log(fullUrl)
 
-    axios.get('https://gateway.marvel.com:443/v1/public/comics?orderBy=title&limit=100&ts=1&apikey=01907a0e43ea09e2dd60256479542cd3&hash=594983bea9a4bb32904aead39c6b11cf')
-    .then((responseComics) => {
-        const comics = responseComics.data.data.results
-        comics_array = []
+        axios.get(fullUrl)
+        .then((responseComics) => {
+            
+            const comics = responseComics.data.data.results
+                    
+            comics.forEach(element => {
 
-        let bar = new progress('Téléchargement \: :percent', { total: 100 });  
+                if (Object.values(answers) == element.title) {
+                    
+                    let bar = new progress('Téléchargement \: :percent', { total: 100 });
                     let timer = setInterval(function () {
                     bar.tick();
+                        
                         if (bar.complete) {
-                            console.log(comics_array)
+                            console.log("\nID : " + element.id + " | Comics : " + element.title)
+                            // console.log("\nDescription : " + element.description)
+                            // console.log("\nComics disponible : " + element.comics.available)
                             clearInterval(timer);
                         }
                     }, 100);
+                }
 
-        comics.forEach((element) => {
-            comics_array.push("ID : " + element.id + " | " + element.title)
-        }); 
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-
-} else if (program.series) {
-
-    axios.get('https://gateway.marvel.com:443/v1/public/series?orderBy=title&limit=100&ts=1&apikey=01907a0e43ea09e2dd60256479542cd3&hash=594983bea9a4bb32904aead39c6b11cf')
-    .then((responseSeries) => {
-        const series = responseSeries.data.data.results
-        series.forEach((element) => {
-        series_array = [element.title] 
-        console.log('Séries : ' + series_array)
-        }); 
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+            });
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+})
 
 } else {
     program.help()
 }
-
-// Interface Inquirer
 
 
 
